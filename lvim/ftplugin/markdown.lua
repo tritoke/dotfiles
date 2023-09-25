2fc92cee25d0f175b730c85d67e4a9f8cbc83e3b
Sam Leonard <samuelleonard35@gmail.com>
2023-09-25 23:30:16 +0100

    ✨ Added vim Rust IDE config stuff.

--- lvim/ftplugin/markdown.lua
+++ lvim/ftplugin/markdown.lua
@@ -1,11 +1,11 @@
-vim.cmd.nnoremap("<LocalLeader>p", [[<cmd>:!pandoc % -s -o "%:r".pdf<enter>]])
-vim.cmd.nnoremap("<localleader>x", [[<cmd>:!pandoc % -s --pdf-engine=xelatex -o "%:r".pdf<enter>]])
-vim.cmd.nnoremap("<localleader>j", [[<cmd>:!pandoc % -V monofont="JetBrains Mono NL Medium" --pdf-engine=xelatex -s -o "%:r".pdf<enter>]])
-vim.cmd.nnoremap("<localleader>v", [[<cmd>:!zathura "%:r".pdf & disown<enter><enter>]])
+vim.api.nvim_set_keymap("n", "<LocalLeader>p", [[<cmd>:!pandoc % -s -o "%:r".pdf<enter>]], { noremap = true })
+vim.api.nvim_set_keymap("n", "<localleader>x", [[<cmd>:!pandoc % -s --pdf-engine=xelatex -o "%:r".pdf<enter>]], { noremap = true })
+vim.api.nvim_set_keymap("n", "<localleader>j", [[<cmd>:!pandoc % -V monofont="JetBrains Mono NL Medium" --pdf-engine=xelatex -s -o "%:r".pdf<enter>]], { noremap = true })
+vim.api.nvim_set_keymap("n", "<localleader>v", [[<cmd>:!zathura "%:r".pdf & disown<enter><enter>]], { noremap = true })
 
 -- j and k move within a wrapped line, useful when writing text.
-vim.cmd.nnoremap("j", "gj")
-vim.cmd.nnoremap("k", "gk")
+vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true })
+vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true })
 
 -- set text wrapping
 vim.opt.wrap = true
