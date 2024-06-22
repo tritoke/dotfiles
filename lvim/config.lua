cd354cf5534714f903e3d8ea98b1f65d26dc1f20
Sam Leonard <samuelleonard35@gmail.com>
2024-06-22 11:38:49 +0100

    ⬆ Update to rustaceanvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -18,11 +18,10 @@ lvim.plugins = {
       require("colorizer").setup()
     end
   },
-  -- Rust Plugins {{{
-  "simrat39/rust-tools.nvim",
+  -- Rust Plugins (mostly bastardised from LazyVim:/lura/lazyvim/plugins/extra/lang/rust.lua) {{{
   {
     "saecki/crates.nvim",
-    version = "v0.3.0",
+    version = "v0.4.0",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("crates").setup {
@@ -36,10 +35,63 @@ lvim.plugins = {
       }
     end,
   },
+  -- Setup rust tooling
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4", -- Recommended
+    ft = { "rust" },
+    opts = {
+      tools = {
+        executor = require("rustaceanvim/executors").termopen, -- termopen / quickfix / toggleterm / vimux
+        runnables = {
+          use_telescope = true,
+        },
+        hover_actions = {
+          border = "rounded",
+        },
+        on_initialized = function()
+          vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+            pattern = { "*.rs" },
+            callback = function()
+              local _, _ = pcall(vim.lsp.codelens.refresh)
+            end,
+          })
+        end,
+      },
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+              unsetTest = { "core", "server" },
+            },
+            lens = {
+              enable = true,
+            },
+            checkOnSave = {
+              enable = true,
+              command = "clippy",
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+      if vim.fn.executable("rust-analyzer") == 0 then
+        LazyVim.error(
+          "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
+          { title = "rustaceanvim" }
+        )
+      end
+    end,
+  },
   {
     "j-hui/fidget.nvim",
-    -- legacy tag commit ID, lazy thinks tag is deprecated and to replace with version but doesn't respect version?? 🙃
-    commit = "0ba1e16d07627532b6cae915cc992ecac249fb97",
     event = "LspAttach",
     opts = {},
   },
@@ -48,106 +100,6 @@ lvim.plugins = {
 -- }}}
 
 lvim.colorscheme = "ronny"
-
--- rust settings {{{
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
-
-local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
-
-local codelldb_path = mason_path .. "bin/codelldb"
-local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb.so"
-
--- rust-tools setup {{{
-local opts = {
-  tools = {
-    executor = require("rust-tools/executors").termopen, -- termopen / quickfix / toggleterm / vimux
-    runnables = {
-      use_telescope = true,
-    },
-    hover_actions = {
-      border = "rounded",
-    },
-    on_initialized = function()
-      vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-        pattern = { "*.rs" },
-        callback = function()
-          local _, _ = pcall(vim.lsp.codelens.refresh)
-        end,
-      })
-    end,
-  },
-  dap = {
-    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-  },
-  server = {
-    on_attach = function(client, bufnr)
-      require("lvim.lsp").common_on_attach(client, bufnr)
-      local rt = require "rust-tools"
-      vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-    end,
-
-    capabilities = require("lvim.lsp").common_capabilities(),
-    settings = {
-      ["rust-analyzer"] = {
-        cargo = {
-          unsetTest = { "core", "server" },
-        },
-        lens = {
-          enable = true,
-        },
-        checkOnSave = {
-          enable = true,
-          command = "clippy",
-        },
-      },
-    },
-  },
-}
-require("rust-tools").setup(opts)
--- }}}
-
--- Rust DAP setup {{{
-lvim.builtin.dap.on_config_done = function(dap)
-  dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-  dap.configurations.rust = {
-    {
-      name = "Launch file",
-      type = "codelldb",
-      request = "launch",
-      program = function()
-        return vim.fn.input({"Path to executable: ", vim.fn.getcwd() .. "/", "file"})
-      end,
-      cwd = "${workspaceFolder}",
-      stopOnEntry = false,
-    },
-  }
-end
--- }}}
-
--- rust which_key bindings {{{
-lvim.builtin.which_key.mappings["C"] = {
-  name = "Rust",
-  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
-  t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
-  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
-  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
-  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
-  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
-  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
-  R = {
-    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
-    "Reload Workspace",
-  },
-  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
-  y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
-  P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
-  i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
-  f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
-  D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
-}
--- }}}
-
--- }}}
 
 -- formatter setup {{{
 
@@ -213,4 +165,3 @@ lvim.keys.normal_mode["<leader>me"] = [["zyiw:exe "tabe man://".@z.""<enter>]]
 vim.opt.undolevels = 10000
 vim.opt.undodir = "/tmp/tritoke/vim_undo"
 vim.opt.undofile = true
-
